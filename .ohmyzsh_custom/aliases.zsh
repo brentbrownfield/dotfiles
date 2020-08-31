9075c460a76549836e7acfdf7d420cc35d81d08a
Brent Brownfield <bbrownfield@gmail.com>
2020-08-31 16:49:10 -0400

    Added alias glo (git log), which outputs one line git log with author and how long ago change was made

--- .ohmyzsh_custom/aliases.zsh
+++ .ohmyzsh_custom/aliases.zsh
@@ -6,3 +6,4 @@ alias -s txt=st
 alias -s json=st
 alias vl='less *.log(.om[1])'
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias glo='/usr/bin/git log --pretty=format:"%h - %an, %<(15)%ar : %s"'
