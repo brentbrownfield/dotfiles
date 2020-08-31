0e2a66f025de324ae6b56eaf74130eb54c9070c9
Brent Brownfield <bbrownfield@gmail.com>
2020-08-31 16:51:55 -0400

    Add alias sdu 'sorted disk usage' to sort output of du command with max depth 1

--- .ohmyzsh_custom/aliases.zsh
+++ .ohmyzsh_custom/aliases.zsh
@@ -7,3 +7,4 @@ alias -s json=st
 alias vl='less *.log(.om[1])'
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias glo='/usr/bin/git log --pretty=format:"%h - %an, %<(15)%ar : %s"'
+alias sdu='du -h --max-depth 1 | sort -h'
