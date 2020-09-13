d387e0a8cd022295d4709a0c67b161048702f11b
Brent Brownfield <bbrownfield@gmail.com>
2020-09-13 12:02:39 -0400

    Add aliases to reload the shell and display sorted 'df -h' output
    Added alias reload
    
    Added alias dfh
    
    Add function body which strips the header line of output, performs
    the original command and then outputs the original header followed
    by the output of the command with the stripped input.

--- .ohmyzsh_custom/functions.zsh
+++ .ohmyzsh_custom/functions.zsh
@@ -46,3 +46,12 @@ dockerImages() {
         docker images | awk '{print $1 ":" $2}';
     fi
 }
+
+# print the header (the first line of input)
+# and then run the specified command on the body (the rest of the input)
+# use it in a pipeline, e.g. ps | body grep somepattern
+body() {
+    IFS= read -r header
+    printf '%s\n' "$header"
+    "$@"
+}
