3cc6960c70253fac4cf1bd5fb36b6dedf0b7d01e
Brent Brownfield <brent@brownfield.soy>
2021-10-15 08:29:10 -0400

    Correct fish function verify256SUM to get correct parameter

--- .config/fish/functions/verifySHA256.fish
+++ .config/fish/functions/verifySHA256.fish
@@ -1,3 +1,3 @@
-function verifySHA256 --argument-names $shafile --description "Verify a SHA256 checksum from file"
+function verifySHA256 --argument-names shafile --description "Verify a SHA256 checksum from file"
     echo (cat $shafile.sha256)"  $shafile" | shasum -a 256 -c
 end
