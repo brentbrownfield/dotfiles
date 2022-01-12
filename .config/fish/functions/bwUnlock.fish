8587d6c73a7f421484def95efa26b4ca271a5d9d
Brent Brownfield <brent@brownfield.soy>
2022-01-12 13:00:32 -0500

    Update fish function for unlocking bitwarden to allow the use of the env variable for password

--- .config/fish/functions/bwUnlock.fish
+++ .config/fish/functions/bwUnlock.fish
@@ -1,3 +1,7 @@
 function bwUnlock --description "Unlock the bitwarden vault"
-    set -xU BW_SESSION (bw unlock --raw (read -s -P "Master Password: "))
+    if set -q BW_PASSWORD
+        set -xU BW_SESSION (bw unlock --raw --passwordenv BW_PASSWORD)
+    else
+        set -xU BW_SESSION (bw unlock --raw (read -s -P "Master Password: "))
+    end
 end
